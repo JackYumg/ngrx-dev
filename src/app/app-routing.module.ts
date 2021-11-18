c8c2d793c96098d4b75162a8d675d7d0aa7ee24f
wethin <806769481@qq.com>
2021-11-18 15:46:38 +0800

    update:angular核心包更新

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,7 +13,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { useHash: environment.useHash }),
+    RouterModule.forRoot(routes, { useHash: environment.useHash, relativeLinkResolution: 'legacy' }),
     StoreModule.forRoot({
       router: routerReducer,
     }),
